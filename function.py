a68647b90cb77e9485e13c2486adf62ede425f48
Sumit Verma <sumit.brancosoft@gmail.com>
2021-10-28 15:55:19 +0530

    sc

---
+++ function.py
@@ -0,0 +1,37 @@
+# def is_palindrom(word) :
+#     reversed_string = word[::-1]
+#     if word == reversed_string :
+#         return True
+#     else :
+#         False
+
+# print(is_palindrom('naman'))
+
+# # fibonacci series
+# # 0, 1, 1, 2, 3, 5, 8, 13...
+# def fibonacci(n) :
+#     first_num, second_num = 0, 1
+#     result = []
+#     for i in range(1, n+1) :
+#         if i == 1 :
+#             result.append(first_num)
+#         elif i == 2 :
+#             result.append(second_num)
+#         else :
+#             last_num = first_num + second_num
+#             result.append(last_num)
+#             first_num = second_num
+#             second_num = last_num
+#     return result
+    
+# print(fibonacci(10))
+
+
+# def default_params(name, age=18, hobby=None):
+#     print(name)
+#     print(age)
+#     print(hobby)
+
+# default_params('sumit')
+# default_params('sumit', 25, 'cricket')
+
---
+++ list.py
@@ -0,0 +1,26 @@
+# ordered collection of items
+
+# list = ['name', 12, 2.3, None]
+# # append, extend, insert
+# # pop, remove, del
+# list.append('newitem')
+# list.extend([1,2])
+# list.insert(2, 'nww')
+# list.pop()
+# list.pop(1) # delete on index
+# del list[1] # delete on index
+# list.remove('anme')
+# print(list)
+
+# list = [8, 1, 3, 5, 3, 4, 22]
+# if 1 in list :
+#     print(True)
+# print(list.count(3))  # count of 3 in list
+# list.reverse()  # reverse list
+# print(sorted(list)) # print in sorted order without sorting
+# list.sort() # sort list
+# list.clear()    # clear list
+# print(list.copy())  # copy list
+# print(list)
+
+
